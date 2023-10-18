2cdb0c8104b375a79562022dbeab1fbe866708cd
zzovun <zzovun@gmail.com>
2023-10-18 10:37:19 +0900

    two

--- src/f202014077/Main.java
+++ src/f202014077/Main.java
@@ -2,6 +2,7 @@ package f202014077;
 
 public class Main {
 	public static void main(String[] args) {
+		Two.print();
 		System.out.println("one 202014077");
 	}
 }
---
+++ src/f202014077/Two.java
@@ -0,0 +1,8 @@
+package f202014077;
+
+public class Two {
+	
+	public static void print() {
+		System.out.println("two");
+	}
+}
