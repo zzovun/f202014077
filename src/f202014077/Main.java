365b8eeadfcde4862d5c85821b2bce27d4e34182
zzovun <zzovun@gmail.com>
2023-10-18 10:40:38 +0900

    three

--- src/f202014077/Main.java
+++ src/f202014077/Main.java
@@ -3,5 +3,6 @@ package f202014077;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("one 202014077");
-	}
+		Three.print()
+;	}
 }
---
+++ src/f202014077/Three.java
@@ -0,0 +1,8 @@
+package f202014077;
+
+public class Three {
+	
+	public static void print() {
+		System.out.println("three");
+	}
+}
